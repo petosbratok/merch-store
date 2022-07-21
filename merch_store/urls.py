2ff1223989e581f30da30fcbfbd8862929ff0028
petosbratok <44753312+petosbratok@users.noreply.github.com>
2022-07-21 14:29:01 +0300

    Commit mistake solved

--- merch_store/urls.py
+++ merch_store/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import url
+from django.urls import re_path, path, include
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf import settings
@@ -9,6 +8,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
